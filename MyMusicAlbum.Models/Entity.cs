60b2a77ba27d5be83a72926e9481e8a271b8e143
Vinicius Lima <82112071+Viny-lima@users.noreply.github.com>
2022-01-21 13:43:54 -0300

    Adicionar arquivos de projeto.

---
+++ MyMusicAlbum.Models/Entity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace MyMusicAlbum.Models
+{
+    public abstract class Entity
+    {
+        public int Id { get; protected set; }
+
+    }
+}
---
+++ MyMusicAlbum/Program.cs
@@ -0,0 +1,15 @@
+﻿using MyMusicAlbum.Models;
+using System;
+
+namespace MyMusicAlbum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Entity myObjetc = new Music();          
+
+            Console.WriteLine(myObjetc);
+        }
+    }
+}
